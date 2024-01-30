3bca3307a1f12542783c32175781b300691b9ca3
Daniel-Tran-247 <baophuc247@gmail.com>
2024-01-30 13:54:33 -0500

    Ready to deploy on Vercel

--- new_GSM/settings.py
+++ new_GSM/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.x/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o!rx1=snmqi1fr0&lav@%%i1tnl06)tfr2r#e!f@8zs9xg^nyh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -98,16 +98,16 @@ WSGI_APPLICATION = "new_GSM.wsgi.application"
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'GSM Local',
-        'USER': 'postgres',
-        'PASSWORD': 'qweqweqwe',
-        'HOST': 'localhost',
-        'PORT': 5433,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "GSM Local",
+        "USER": "postgres",
+        "PASSWORD": "qweqweqwe",
+        "HOST": "localhost",
+        "PORT": 5433,
     }
 }
- 
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -168,4 +168,3 @@ DJOSER = {
         "current_user": "core.serializers.UserSerializer",
     }
 }
-
--- new_GSM/wsgi.py
+++ new_GSM/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_GSM.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "new_GSM.settings")
 
 application = get_wsgi_application()
+
+app = application

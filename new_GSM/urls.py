ddc4b62258e6f3c5acec54b93e0b887b258f758f
Daniel-Tran-247 <baophuc247@gmail.com>
2024-01-31 08:14:05 -0500

    Build Static Files

--- new_GSM/urls.py
+++ new_GSM/urls.py
@@ -3,17 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# collection for image
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("test_app.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # collection for image
